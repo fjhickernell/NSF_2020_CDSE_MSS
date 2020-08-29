4b1bc66db9f13b5a8091d6f1ad62df46a00c667d
Fred Hickernell <hickernell@iit.edu>
2020-08-28 21:22:25 -0500

    Home computer update

--- ProgramsImages/placeHolderPlot.m
+++ ProgramsImages/placeHolderPlot.m
@@ -4,8 +4,12 @@ function placeHolderPlot(plotname)
 
 close all
 
+if nargin < 1
+   plotname = 'Sample';
+end
+
 figure
 axis([0 1 0 1])
-text(0.1,0.35,'Place Holder','fontsize',40)
-text(0.1,0.65,plotname,'fontsize',40)
+text(0.1,0.35,'Place Holder','fontsize',40,'color','blue')
+text(0.1,0.65,plotname,'fontsize',40,'color','blue')
 print('-dpng',[plotname '.png'])
